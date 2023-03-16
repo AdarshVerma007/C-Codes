f021226c65d4c96ceebf60890b85626acf9d8d2a
Adarsh Verma <124288995+AdarshVerma007@users.noreply.github.com>
2023-03-16 11:05:58 +0530

    Update Find leap year by conditional operator.c

--- Find leap year by conditional operator.c
+++ Find leap year by conditional operator.c
@@ -6,7 +6,7 @@ int main()
 {
  int a,b,c;
  scanf("%d",&a);
-  (a%400==0) || (a%4==0) && (a%100!==0) ? printf("Leap Year"): printf("Non Leap Year");
+  (a%400==0) || (a%4==0) && (a%100!=0) ? printf("Leap Year"): printf("Non Leap Year");
 
     return 0;
 }
